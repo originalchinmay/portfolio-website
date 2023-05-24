d58d3432019bac34c01fd1cf1b3b7f1d9f75f98a
originalchinmay <chinmayjj97@gmail.com>
2023-05-24 22:30:39 +0530

    Updated code for card functionality

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
       window.location.href = './portfolio.html';
     });
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const portfolioCard = document.getElementById('about-me');
+    
+  portfolioCard.addEventListener('click', function() {
+    window.location.href = './aboutme.html';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const portfolioCard = document.getElementById('contact-me');
+    
+  portfolioCard.addEventListener('click', function() {
+    window.location.href = './contactme.html';
+  });
+});
